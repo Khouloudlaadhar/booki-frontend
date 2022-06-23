3c0ab7dbed12741ced6dafe3f6e89175e81b5c98
khouloud Laadhar <khouloudlaadhar@gmail.com>
2022-06-23 07:11:36 +0100

    Modify image height in the carousel

--- src/Components/Carousel.js
+++ src/Components/Carousel.js
@@ -16,22 +16,34 @@ export const Carousel = () => {
           <CDBCarouselInner>
             <CDBCarouselItem itemId="1">
               <CDBView>
-                <img className="d-block w-100 h-50" src="https://image.resabooking.com/images/hotel/Le_Royal_Hammamet_.jpg" alt="First slide" />
+                <img height={400}
+                className="d-block w-100 " 
+                src="https://image.resabooking.com/images/hotel/Le_Royal_Hammamet_.jpg" 
+                alt="First slide" />
               </CDBView>
             </CDBCarouselItem>
             <CDBCarouselItem itemId="2">
               <CDBView>
-                <img className="d-block w-100 h-50" src="https://image.resabooking.com/images/image_panoramique/El_Mouradi_Hammamet_2.jpg" alt="Second slide" />
+                <img height={400}
+                  className="d-block w-100 " 
+                  src="https://1503846100.rsc.cdn77.org/photos/4556/Hotel-Steigenberger-Marhaba-Thalasso-Hammamet-Vue-Piscine3.jpg" 
+                  alt="Second slide" />
               </CDBView>
             </CDBCarouselItem>
             <CDBCarouselItem itemId="3">
               <CDBView>
-                <img className="d-block w-100 h-50" src="https://vive-media-tpvbooking.netdna-ssl.com/63482/0.jpg" alt="Third slide" />
+                <img height={400}
+                className="d-block w-100 " 
+                src="https://vive-media-tpvbooking.netdna-ssl.com/63482/0.jpg" 
+                alt="Third slide" />
               </CDBView>
             </CDBCarouselItem>
             <CDBCarouselItem itemId="4">
               <CDBView>
-                <img className="d-block w-100 h-50" src="https://images.trvl-media.com/hotels/3000000/2240000/2239000/2238909/62b3f434.jpg?impolicy=resizecrop&rw=670&ra=fit" alt="Third slide" />
+                <img height={400}
+                  className="d-block w-100 "
+                  src="https://images.trvl-media.com/hotels/3000000/2240000/2239000/2238909/62b3f434.jpg?impolicy=resizecrop&rw=670&ra=fit"
+                  alt="fourth slide" />
               </CDBView>
             </CDBCarouselItem>
           </CDBCarouselInner>
